221df3e35d83272b0de91082b48ec6ef41714898
Jamil <Jamil.Dabbaghian@gmail.com>
2022-08-05 09:40:39 +0430

    the functions works manually for two steps, iteration not done yet

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,39 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import random as rnd
+# if 2 or more cells are alive the cell will remain alive/come to life
 
-print(np.array([[1, 2, 3], [3, 2, 1]]))
+field1 = np.zeros((10, 10), np.int8)
+
+field1[3:5, 1] = 1
+
+shape = np.shape(field1)
+field2 = np.zeros(shape = shape)
+
+num_neighbor = 1
+
+for i in range(shape[0]):
+    for j in range(shape[1]):
+
+        indx = [i, j]
+        
+        left = max(0, indx[0]-num_neighbor)
+        right = max(0, indx[0]+num_neighbor+1)
+
+        bottom = max(0,indx[1]-num_neighbor)
+        top = max(0,indx[1]+num_neighbor+1)
+
+        selection = field1[left:right, bottom:top]
+
+        if np.sum(selection) >= 2:
+            field2[i, j] = 1
+
+print(field1, "\n", field2)
+
+plt.imshow(field1)
+plt.show()
+
+plt.imshow(field2)
+plt.show()
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import random as rnd
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([[1, 1, 0], [0, 0, 1]])
+
+print(np.sum(x))

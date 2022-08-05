fb875a5e8ef47a4fa3e3aedede44046e98830b43
Jamil <Jamil.Dabbaghian@gmail.com>
2022-08-05 15:18:01 +0430

    the algorithm works, and the function now generates the plots (as a series of individual plots)

--- game_of_life.py
+++ game_of_life.py
@@ -30,9 +30,31 @@ fig = plt.figure()
 ax = fig.add_subplot(211)
 
 plt.imshow(field1, cmap='binary')
-ax = fig.add_subplot(221)
+ax = fig.add_subplot(212)
 
 plt.imshow(field2, cmap='binary')
 plt.show()
+
+def game_of_life(x, generations = 100):
+    
+    shape = np.shape(x)
+    next_step = np.zeros(shape = shape)
+    num_neighbor = 1
+
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+
+            indx = [i, j]
+            
+            left = max(0, indx[0]-num_neighbor)
+            right = max(0, indx[0]+num_neighbor+1)
+
+            bottom = max(0,indx[1]-num_neighbor)
+            top = max(0,indx[1]+num_neighbor+1)
+
+            selection = field1[left:right, bottom:top]
+            if np.sum(selection) > 2:
+                next_step[i, j] = 1
+            return(next_step)
 
 
--- test.py
+++ test.py
@@ -1,7 +1,50 @@
 import random as rnd
+from re import L
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([[1, 1, 0], [0, 0, 1]])
+x = np.zeros((10, 10))
+x[1:4, 4] = 1
+x[3, 3] = 1
+x[2, 2] = 1
 
-print(np.sum(x))
+
+def game_of_life(x, generations = 100):
+    
+    first_step = x
+    shape = np.shape(x)
+    num_neighbor = 1
+    plt.imshow(x, cmap='binary')
+    plt.show()
+    for x in range(generations):
+        next_step = np.zeros(shape = shape)
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+
+                indx = [i, j]
+                
+                left = max(0, indx[0]-num_neighbor)
+                right = max(0, indx[0]+num_neighbor+1)
+
+                bottom = max(0,indx[1]-num_neighbor)
+                top = max(0,indx[1]+num_neighbor+1)
+
+                selection = first_step[left:right, bottom:top]
+                sum = (np.sum(selection))- first_step[i, j]
+                                
+                if first_step[i, j] == 1 and (sum == 2 or sum == 3):
+                    next_step[i, j] = 1
+                elif first_step[i, j] == 0 and (sum == 3): 
+                    next_step[i, j] = 1
+                else:
+                    next_step[i, j] = 0
+
+        first_step = next_step
+        print(x)
+        plt.imshow(first_step, cmap='binary')
+        plt.show()
+        print(next_step, "\n")
+        
+game_of_life(x)
+
+

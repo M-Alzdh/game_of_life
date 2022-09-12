74ffce9cf468880b800ac8c7ef97bd6bd5a84d21
Mohamad <Jamil.Dabbaghian@gmail.com>
2022-09-12 20:50:07 +0430

    window selection now works

--- smooth_life.py
+++ smooth_life.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -6,25 +7,32 @@ import matplotlib.animation as animation
 ## it calculates all states to be plotted first and then plots them
 
 ## importing inital states from 'ready_input_csv
-with open(r'game_of_life\ready_input_csv\acorn.csv') as file_name:
+with open(r'game_of_life\ready_input_csv\worms.csv') as file_name:
     input = np.loadtxt(file_name, delimiter=',')
 
-def clip(x):
-    if x<-1:
-        return -1
-    if x>1:
-        return 1
-    return x
+filter_array = np.array(([0.8, -0.85, 0.8], [-0.85, -0.2, -0.85], [0.8, -0.85, 0.8]))
+filter_slime = np.array(([0.8, -0.85, 0.8], [-0.85, -0.2, -0.85], [0.8, -0.85, 0.8]))
+filter_waves = np.array(([0.565, -0.716, 0.565], [-0.716, 0.627, -0.716], [0.565, -0.716, 0.565]))
 
-def activation_fun(x):
+def activation_func(x):
+    return -1/(0.89*(x**2)+1)+1
+
+def waves_activation(x):
     return abs(1.2*x)
 
-## generating a random inital state
-x = np.random.choice(2, (10, 10), p = [0.8, 0.2])
-padded = np.pad(input, ((100, 100), (100, 100)), 'constant', constant_values = (0, 0) )
+def clip(x):
+    if x>1:
+        return 1
+    if x<-1:
+        return -1
+    return x
 
+## generating a random inital state
+x = np.random.choice(2, (200, 200), p = [0.8, 0.2])
+init_worm = np.random.choice([0, 1], (50, 50), p = (0.8, 0.2))
+padded = np.pad(x, ((1, 1), (1, 1)), 'constant', constant_values = (0, 0) )
 #set the number of generations
-n_generations = 500
+n_generations = 1000
 
 def game_of_life(x, generations = 10):
     imagelist = []
@@ -34,8 +42,8 @@ def game_of_life(x, generations = 10):
     num_neighbor = 1
     for x in range(generations):
         next_step = np.zeros(shape = shape)
-        for i in range(shape[0]):
-            for j in range(shape[1]):
+        for i in range(1,shape[0]-1):
+            for j in range(1, shape[1]-1):
 
                 indx = [i, j]
                 
@@ -46,14 +54,11 @@ def game_of_life(x, generations = 10):
                 top = max(0,indx[1]+num_neighbor+1)
 
                 selection = first_step[left:right, bottom:top]
-                sum = (np.sum(selection))- first_step[i, j]
+                filtered = selection*filter_waves
+                sum = np.sum(filtered)#- first_step[i, j]
+                activated = clip(waves_activation(sum))
                                 
-                if first_step[i, j] == 1 and (sum == 2 or sum == 3):
-                    next_step[i, j] = 1
-                elif first_step[i, j] == 0 and sum == 3: 
-                    next_step[i, j] = 1
-                else:
-                    next_step[i, j] = 0
+                next_step[i, j] = activated
         first_step = next_step
         imagelist.append(next_step)
     return imagelist
@@ -61,15 +66,23 @@ def game_of_life(x, generations = 10):
 ## use "input" for imported initial state
 state_arrays = game_of_life(padded, n_generations)
 
+
+
 fig = plt.figure()
-plot = plt.imshow(state_arrays[0], cmap='binary')
+plot = plt.imshow(state_arrays[0], cmap='copper')
+ax = plt.gca()
+
+ax.axes.xaxis.set_visible(False)
+ax.axes.yaxis.set_visible(False)
 
 def update_plot(j):
     plot.set_array(state_arrays[j])
     return [plot]
 
-ani = animation.FuncAnimation(fig, update_plot, frames=range(n_generations), interval = 50, blit = True)
+ani = animation.FuncAnimation(fig, update_plot, frames=range(n_generations), interval = 100, blit = True)
 plt.show()
 
 
-
+f = r"c://Users/Haji/Desktop/animation3.gif" 
+writergif = animation.PillowWriter(fps=30) 
+ani.save(f, writer=writergif)
